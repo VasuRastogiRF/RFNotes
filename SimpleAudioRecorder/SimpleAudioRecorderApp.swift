41469898802bd91241116535ee2adb92d071f674
VasuRastogiRF <vasu.rastogi@rocketfrog.ai>
2026-02-02 07:15:54 +0530

    Initial Commit

---
+++ SimpleAudioRecorder/SimpleAudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAudioRecorderApp.swift
+//  SimpleAudioRecorder
+//
+//  Created by Vasu Rastogi on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
